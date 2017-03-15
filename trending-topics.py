5084cc6e48b0043b3cb448fdf8a144ca0909935e
Carol Schmitz <carolgrrr@gmail.com>
2017-03-15 16:16:55 -0500

    add wordpress posting functions

--- trending-topics.py
+++ trending-topics.py
@@ -5,7 +5,55 @@ import time
 import yagmail
 from datetime import datetime
 from collections import Counter
+from wordpress_xmlrpc import Client, WordPressPost
+from wordpress_xmlrpc.methods.posts import GetPosts, NewPost
+from wordpress_xmlrpc.methods.users import GetUserInfo
+from wordpress_xmlrpc.compat import xmlrpc_client
+from wordpress_xmlrpc.methods import media, posts
+import configparser
 
+def create_wordpress_client(settings_filename):
+    config = configparser.ConfigParser()
+    config.read(settings_filename)
+
+    url = config.get('wordpress', 'url')
+    username = config.get('wordpress', 'user')
+    password = config.get('wordpress', 'pass')
+
+    client = Client(url, username, password)
+
+    return client
+
+def generate_post_content_string(report_filename):
+    content_string = '<table>'
+
+    with open(report_filename, 'r') as tsv_file:
+        for row in tsv_file:
+            cells = row.split('\t')
+            region = cells[9]
+            nation = cells[8]
+            location = cells[0]
+            trend = cells[2]
+            count = cells[5]
+            table_row = '<tr><td>' + region + '</td><td>' + nation + '</td><td>' + location + '</td><td>' + trend + '</td><td>' + count + '</td></tr>'
+            content_string += table_row
+
+    content_string += '</table>'
+    return content_string
+
+def post_report_to_wordpress(settings_filename, report_filename):
+    wp = create_wordpress_client(settings_filename)
+
+    filename = report_filename
+    content_string = generate_post_content_string(filename)
+    title = filename[:-4]
+
+    post = WordPressPost()
+    post.title = title
+    post.content = content_string
+
+    wp.call(NewPost(post))
+    print('posted.')
 
 def get_twitter(config_file):
     """ Read the config_file and construct an instance of TwitterAPI.
