0a768f2c946f4f571849479ff3dcab1c002a4a17
Carol Schmitz <carolgrrr@gmail.com>
2017-03-21 15:59:29 -0500

    attempt to add collapsible

--- trending-topics.py
+++ trending-topics.py
@@ -223,8 +223,8 @@ def create_wordpress_client(settings_filename):
     return client
 
 def generate_post_content_string(report_filename, sort_by):
-    content_string = '<script type="text/javascript">function myFunction() {var x = document.getElementById(\'myDIV\');if (x.style.display === \'none\') {x.style.display = \'block\';} else {x.style.display = \'none\';} }</script>'
-    content_string += '<table>'
+    #content_string = '<script type="text/javascript">function myFunction() {var x = document.getElementById(\'myDIV\');if (x.style.display === \'none\') {x.style.display = \'block\';} else {x.style.display = \'none\';} }</script>'
+    content_string = '<table>'
 
     with fileinput.input(files=report_filename) as tsv_file:
         if sort_by == 'trend':
@@ -238,7 +238,7 @@ def generate_post_content_string(report_filename, sort_by):
         location = cells[0]
         trend = cells[2]
         count = cells[5]
-        table_row = '<tr><td><input type="checkbox" name="' + trend +'"></td><td>' + region + '</td><td>' + nation + '</td><td>' + location + '</td><td>' + trend + '</td><td>' + str(count) + '</td><td><button onclick="myFunction()" id="myDIV">+</button></td></tr>'
+        table_row = '<div id="'+ region + '" class="collapse"><tr><td><input type="checkbox" name="' + trend +'"></td><td>' + region + '</td><td>' + nation + '</td><td>' + location + '</td><td>' + trend + '</td><td>' + str(count) + '</td><td><button data-toggle="collapse" data-target="' + region + '">+</button></td></tr>'
         content_string += table_row
 
     content_string += '</table>'
@@ -290,7 +290,7 @@ def sort_by_location(tsv):
         #    continue
         row[5] = int(row[5])
 
-    # x[5] = count x[2] = trend, x[0] = location, x[8] = nation, x[9] = region
+    # x[9] = region, x[8] = nation, x[0] = location, x[2] = trend, x[5] = count
     sorted_rows = sorted(rows, key = lambda x: (x[9], x[8], x[0], x[2], -x[5]))
     return sorted_rows
 
