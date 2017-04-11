e75b13c87eff96051df424a19b0ad15d720477b5
Carol Schmitz <carolgrrr@gmail.com>
2017-04-10 21:38:19 -0500

    woocommerce api calls

---
+++ shopping-cart.py
@@ -0,0 +1,38 @@
+import configparser
+from woocommerce import API
+
+
+def main():
+	config = configparser.ConfigParser()
+	config.read('settings.cfg')
+
+	wcapi = API(
+    	url=config.get('wordpress', 'url'), # Your store URL
+    	consumer_key=config.get('wordpress', 'consumer_key'), # Your consumer key
+    	consumer_secret=config.get('wordpress', 'consumer_secret'), # Your consumer secret
+    	wp_api=True, # Enable the WP REST API integration
+    	version="wc/v2" # WooCommerce WP REST API version
+	)
+
+
+	print(wcapi)
+	#print(wcapi.get('orders/40856').json())
+
+	print(wcapi.get('orders/40856'))
+
+	#/wp-json/wc/v2/orders
+	#data = {
+	#	"id": 123
+	#	"line_items": [
+    #    {
+    #        "product_id": 93,
+    #        "quantity": 10
+    #    }
+    #	]
+	#}
+
+	#print(wcapi.post('orders', data).json())
+
+
+if __name__ == "__main__":
+	main()
