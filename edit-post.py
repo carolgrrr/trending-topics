ea6c25a80d817f3d6b5a2788e3e660700c4634ed
Carol Schmitz <carolgrrr@gmail.com>
2017-04-30 16:25:42 -0500

    add data prep functions

--- edit-post.py
+++ edit-post.py
@@ -6,6 +6,68 @@ from wordpress_xmlrpc.methods import media, posts
 import configparser
 
 
+### DATA PREP FUNCTIONS ###
+
+def get_datestring():
+    today = datetime.today()
+    year, month, day = today.year, today.month, today.day
+
+    if len(str(month)) < 2:
+        month = "0%d" %(month)
+    else:
+        month = "%d" %(month)
+    
+    if len(str(day)) < 2:
+        day = "0%d" %(day)
+    else:
+        day = "%d" %(day)
+
+    datestring = "%d-%s-%s" %(year, month, day)
+    return datestring
+
+def sort_by_trend_count(tsv):
+    today = get_datestring()
+    rows = []
+
+    for row in tsv:
+        if not fileinput.isfirstline():
+            cells = row.split('\t')
+            if cells[0] == today:
+                del(cells[0])
+                rows.append(cells)
+
+    for row in rows:
+        #if row == rows[0]:
+        #    continue
+        row[5] = int(row[5])
+
+    # x[5] = count x[2] = trend, x[0] = location, x[8] = nation, x[9] = region
+    sorted_rows = sorted(rows, key = lambda x: (-x[5], x[2], x[9], x[8], x[0]))
+    return sorted_rows
+
+def sort_by_location(tsv):
+    today = get_datestring()
+    rows = []
+
+    for row in tsv:
+        if not fileinput.isfirstline():
+            cells = row.split('\t')
+            if cells[0] == today:
+                del(cells[0])
+                rows.append(cells)
+
+    for row in rows:
+        #if row == rows[0]:
+        #    continue
+        row[5] = int(row[5])
+
+    # x[5] = count x[2] = trend, x[0] = location, x[8] = nation, x[9] = region
+    sorted_rows = sorted(rows, key = lambda x: (x[9], x[8], x[0], x[2], -x[5]))
+    return sorted_rows
+
+
+### WORDPRESS FUNCTIONS ###
+
 def create_wordpress_client(settings_filename):
     config = configparser.ConfigParser()
     config.read(settings_filename)
@@ -57,6 +119,8 @@ def post_report_to_wordpress(settings_filename, report_filename, sort_by):
     print('%s posted.' % title)
 
 
+### MAIN ###
+
 def main():
     settings = 'settings.cfg'
     config = configparser.ConfigParser()
@@ -74,9 +138,6 @@ def main():
     	elif post.title == "Today's Top Trending Topics on Twitter":
     		print('replace top trending post with today\'s data')
     	#print(post.title)
-
-
-
 
 
 if __name__ == '__main__':
