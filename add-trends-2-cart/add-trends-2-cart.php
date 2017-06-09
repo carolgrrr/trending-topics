78983a19c2987807a074083283a8778ead07b589
Carol Schmitz <carolgrrr@gmail.com>
2017-06-09 10:46:38 -0500

    wip

--- add-trends-2-cart/add-trends-2-cart.php
+++ add-trends-2-cart/add-trends-2-cart.php
@@ -1,4 +1,12 @@
 <?php
 /**
  * Plugin Name: add-trends-2-cart
+ * Plugin URI: https://connectedaction.net/
+ * Description: Add selected trends to the WooCommerce shopping cart
+ * Version: 0.1
+ * Author: Carol Schmitz
+ * Author URI: https://github.com/carolgrrr
  */
+
+
+function 
