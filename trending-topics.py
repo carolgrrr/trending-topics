7bd5b80a4539b8f59f21520f061f9d23dce284e2
Carol Schmitz <carolgrrr@gmail.com>
2017-06-26 23:32:30 -0500

    remove post_report_to_wordpress()

--- trending-topics.py
+++ trending-topics.py
@@ -289,37 +289,6 @@ def update_wordpress_page(settings_filename, report_filename, sort_by):
 	print('%s posted.' % title)
 
 
-def post_report_to_wordpress(settings_filename, report_filename, sort_by):
-	wp = create_wordpress_client(settings_filename)
-
-	filename = report_filename
-	content_string = generate_content_string(filename, sort_by)
-	if '-17-' in filename:
-		title = 'Today\'s Trending Topics (Containing 17) on Twitter'
-	if '-top-' in filename:
-		title = 'Today\'s Top Trending Topics on Twitter'
-
-
-	post = WordPressPost()
-	post.title = title 
-	post.content = content_string
-	# uncomment below to publish
-	#post.post_status = publish
-	
-	filter_id = ""
-	draft_posts = wp.call(posts.GetPosts({'post_status': 'draft'}))
-
-	for draft in draft_posts:
-		if draft.title == 'Todays Top Trending Topics (Containing 17) on Twitter':
-			filter_id = draft.id
-
-	if filter_id != "":
-		wp.call(posts.EditPost(filter_id, post))
-	else:
-		wp.call(NewPost(post))
-
-	print('%s posted.' % title)
-
 def sort_by_trend_count(tsv):
 	today = get_datestring()
 	rows = []
